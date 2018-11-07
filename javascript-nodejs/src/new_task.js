f5924126fe1fecd50e48e94b84e15ecd6e357ada
Ilko <kachar136@gmail.com>
2018-11-07 11:46:38 +0200

    Migrate buffer constructor to Buffer.from

--- javascript-nodejs/src/new_task.js
+++ javascript-nodejs/src/new_task.js
@@ -8,7 +8,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
     var msg = process.argv.slice(2).join(' ') || "Hello World!";
 
     ch.assertQueue(q, {durable: true});
-    ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+    ch.sendToQueue(q, Buffer.from(msg), {persistent: true});
     console.log(" [x] Sent '%s'", msg);
   });
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
